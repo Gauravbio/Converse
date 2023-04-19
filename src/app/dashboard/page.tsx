640607010f44fb8c18331d79974fb78362a9259a
Gauravbio <93136782+Gauravbio@users.noreply.github.com>
2023-04-19 20:59:41 +0530

    redis

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import Button from '@/components/ui/Button'
+import React from 'react'
+
+interface pageProps{
+
+}
+
+const page: React.FC<pageProps> = ({}) => {
+  return (
+    <Button>
+        Hello
+    </Button>
+  )
+}
+
+export default page
