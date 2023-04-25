a7352585c531b675c32657ebd2821bd5db586d1a
Gauravbio <93136782+Gauravbio@users.noreply.github.com>
2023-04-25 21:52:55 +0530

    babelrc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-import { Inter } from 'next/font/google'
 import Button from '@/components/ui/Button'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default async function Home() {
   return (
